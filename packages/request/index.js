d0cadb1ab9747974a4ab691f6e18045d45c41c20
lizi <530331059@qq.com>
2021-07-02 15:35:55 +0800

    add age

--- packages/request/index.js
+++ packages/request/index.js
@@ -3,9 +3,10 @@
  * @Autor: lizi
  * @Date: 2021-07-02 14:48:32
  * @LastEditors: lizi
- * @LastEditTime: 2021-07-02 14:57:16
+ * @LastEditTime: 2021-07-02 15:35:36
  * @FilePath: \roy-component\packages\request\index.js
  */
 
 const name = '我是封装的request.js';
+const age = 14;
 export default name;
