16fa8d0f498802d22b3cae1b166b486e3e8ffa4a
lizi <530331059@qq.com>
2021-07-02 16:29:21 +0800

    change sex

--- packages/request-ajax/index.js
+++ packages/request-ajax/index.js
@@ -3,11 +3,11 @@
  * @Autor: lizi
  * @Date: 2021-07-02 14:48:32
  * @LastEditors: lizi
- * @LastEditTime: 2021-07-02 16:25:37
+ * @LastEditTime: 2021-07-02 16:29:07
  * @FilePath: \roy-component\packages\request-ajax\index.js
  */
 
 const name = '我是封装的request.js';
 const age = 14;
-const sex = '男';
+const sex = '女';
 export default name;
