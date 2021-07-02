bb96068277251c04b525cbf62e2ef20eb15aeeaf
lizi <530331059@qq.com>
2021-07-02 16:37:53 +0800

    add work

--- packages/request-ajax/index.js
+++ packages/request-ajax/index.js
@@ -3,11 +3,12 @@
  * @Autor: lizi
  * @Date: 2021-07-02 14:48:32
  * @LastEditors: lizi
- * @LastEditTime: 2021-07-02 16:29:07
+ * @LastEditTime: 2021-07-02 16:36:24
  * @FilePath: \roy-component\packages\request-ajax\index.js
  */
 
 const name = '我是封装的request.js';
 const age = 14;
 const sex = '女';
+const work = 'coder';
 export default name;
