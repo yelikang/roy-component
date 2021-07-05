639ba0c16a7d74a7ba33ba63373cd18a47d19c62
lizi <530331059@qq.com>
2021-07-05 16:51:38 +0800

    reset request

--- packages/request-ajax/index.js
+++ packages/request-ajax/index.js
@@ -3,12 +3,9 @@
  * @Autor: lizi
  * @Date: 2021-07-02 14:48:32
  * @LastEditors: lizi
- * @LastEditTime: 2021-07-02 16:36:24
+ * @LastEditTime: 2021-07-05 16:51:10
  * @FilePath: \roy-component\packages\request-ajax\index.js
  */
 
 const name = '我是封装的request.js';
-const age = 14;
-const sex = '女';
-const work = 'coder';
 export default name;
