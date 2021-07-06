ccc078b99d00de62f81bf6f76d0980ddd81ad32e
lizi <530331059@qq.com>
2021-07-06 15:03:46 +0800

    init printer

--- packages/printer/index.js
+++ packages/printer/index.js
@@ -3,6 +3,8 @@
  * @Autor: lizi
  * @Date: 2021-07-02 14:48:19
  * @LastEditors: lizi
- * @LastEditTime: 2021-07-02 14:48:26
+ * @LastEditTime: 2021-07-06 15:02:52
  * @FilePath: \roy-component\packages\printer\index.js
  */
+const name = 'web打印';
+export default name;
