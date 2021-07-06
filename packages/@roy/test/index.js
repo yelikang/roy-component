1efdd80cd42e2d6e2adc803ddbafdef5b51350b5
lizi <530331059@qq.com>
2021-07-06 17:32:04 +0800

    reset roy-test

--- packages/@roy/test/index.js
+++ packages/@roy/test/index.js
@@ -3,8 +3,8 @@
  * @Autor: lizi
  * @Date: 2021-07-06 17:27:33
  * @LastEditors: lizi
- * @LastEditTime: 2021-07-06 17:29:56
+ * @LastEditTime: 2021-07-06 17:31:50
  * @FilePath: \roy-component\packages\@roy\test\index.js
  */
 
-export default 'roy-test1';
+export default 'roy-test';
