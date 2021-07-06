fd3179bf4ad6d629d11fc510890b0a6f6a63e110
lizi <530331059@qq.com>
2021-07-06 17:28:18 +0800

    init roy-test

---
+++ packages/@roy/test/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Description: 
+ * @Autor: lizi
+ * @Date: 2021-07-06 17:27:33
+ * @LastEditors: lizi
+ * @LastEditTime: 2021-07-06 17:27:48
+ * @FilePath: \roy-component\packages\@roy\test\index.js
+ */
+
+export default 'roy-test';
